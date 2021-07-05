3a653d1627b6cf93475657baf8354be7e6b23f4c
FelipeSSac <felipe_adoubs@outlook.com>
2021-07-05 18:00:32 -0300

    deploy teste 1, npm start tsc

---
+++ src/database/migrations/1625517664325-jadson.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class jadson1625517664325 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}

57c1d7243f7d98071d53fcd2872ea6ed38abd404
FelipeSSac <felipe_adoubs@outlook.com>
2021-07-05 18:19:01 -0300

    deploy teste 3, build com tsc em dev 2

--- build/database/connect.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var typeorm_1 = require("typeorm");
-typeorm_1.createConnection().then(function () { return console.log('Successfully connected with database!'); });
